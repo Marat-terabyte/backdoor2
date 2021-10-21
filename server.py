d5c34b95fa428c38fd5ec0347c0928fbca6996a2
Marat-terabyte <marat.amzph21@gmail.com>
2021-10-21 18:56:33 +0000

    add function 'download' and 'upload'

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 import socket
+import time
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(('127.0.0.1', 7777))
+    server.bind(('', 7777))
     server.listen(1)
 
     conn, addr = server.accept()
@@ -19,15 +20,49 @@ def get_data():
     return data
 
 def main():
-    while True:
-        command = str(input('>>>'))
+    try:
+        while True:
+            command = str(input('>>>'))
 
-        if command == '':
-            continue
+            if command == '':
+                continue
 
-        send_data(command)
-        print(get_data())
+            elif 'download' in command:
+                filename = str(input('How will called:'))
+                send_data(command)
 
+                with open(filename, 'wb') as file:
+                    while True:
+                        text = conn.recv(2048)
+
+                        if text == b'ST0P':
+                            break
+                        else:
+                            file.write(text)
+
+
+            elif 'upload' in command:
+                filename = str(input('filename:'))
+                send_data(command)
+
+                with open(filename, 'rb') as file:
+                    content = file.read(2048)
+                    conn.send(content)
+                    print(content)
+                    time.sleep(1)
+
+                    conn.send('ST0P'.encode())
+
+            elif 'q' in command:
+                send_data('q')
+                break
+
+            else:
+                send_data(command)
+                print(get_data())
+
+    except:
+        print('Error(server)')
 
 if __name__ == '__main__':
     main()
